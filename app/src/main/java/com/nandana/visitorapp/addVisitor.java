26d115fb027cf1b2bbb7ba1775fcddef70f06d48
nandana-sivan <nandanasivan.02@gmail.com>
2024-12-19 10:33:36 +0530

    api done

--- app/src/main/java/com/nandana/visitorapp/addVisitor.java
+++ app/src/main/java/com/nandana/visitorapp/addVisitor.java
@@ -13,6 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class addVisitor extends AppCompatActivity {
 
     EditText ed1,ed2,ed3,ed4;
@@ -33,15 +41,49 @@ public class addVisitor extends AppCompatActivity {
         b2=(Button)findViewById(R.id.b2m);
 
         b1.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
                 getFn=ed1.getText().toString();
                 getLn=ed2.getText().toString();
                 getPrps=ed3.getText().toString();
                 getWtm=ed4.getText().toString();
-                Toast.makeText(getApplicationContext(),getFn+" "+getLn+" "+getPrps+" "+getWtm,Toast.LENGTH_LONG).show();
+                 if(getFn.isEmpty()||getLn.isEmpty()||getPrps.isEmpty()||getWtm.isEmpty()) {
+                 Toast.makeText(getApplicationContext(),"values are invalid/empty",Toast.LENGTH_LONG).show();
+                 }
+                 else {
+                    callApi();
+                 }
+                 }
+
+            private void callApi() {
+                String apiUrl="https://log-app-demo-api.onrender.com/addvisitor";
+                JSONObject data= new JSONObject();
+                try {
+                    data.put("firstname",getFn);
+                    data.put("lastname",getLn);
+                    data.put("purpose",getPrps);
+                    data.put("whomToMeet",getWtm);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+               JsonObjectRequest request=new JsonObjectRequest(
+                       Request.Method.POST,
+                       apiUrl,
+                       data,
+                       response -> Toast.makeText(getApplicationContext(),"successfully added",Toast.LENGTH_LONG).show(),
+                       error -> Toast.makeText(getApplicationContext(),"error occured",Toast.LENGTH_LONG).show()
+
+
+               );
+
+                RequestQueue queue= Volley.newRequestQueue(getApplicationContext());
+                queue.add(request);
 
             }
+
+
         });
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
